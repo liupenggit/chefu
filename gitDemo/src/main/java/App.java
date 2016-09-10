6a3fdcd15ff407896465d02a9da444ab54a60c97
lp <1078921890@qq.com>
2016-09-10 13:42:01 +0800

    2222

--- gitDemo/src/main/java/App.java
+++ gitDemo/src/main/java/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
        
+        System.out.println( "Hello World! branch1" );
         System.out.println( "Hello World! branch2" );
     }
 }
