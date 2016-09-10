50b45d55fd5c1d991fd8a09b0fdeb75a7750f8b5
lp <1078921890@qq.com>
2016-09-10 13:00:45 +0800

    1222

--- gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
+++ gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! @@@@@@@@@@@@@@@@@" );
-        System.out.println( "Hello World! 11111111111111111" );
-        System.out.println( "Hello World! 11111111111111111" );
-        System.out.println( "Hello World! 333333333333" );
+        System.out.println( "Hello World! new 11111111111" );
     }
 }
