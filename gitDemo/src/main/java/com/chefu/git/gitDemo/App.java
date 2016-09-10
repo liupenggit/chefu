692c14a5c81f457ced68a51c0fde67e27909253b
lp <1078921890@qq.com>
2016-09-10 13:01:25 +0800

    11111111

--- gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
+++ gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
@@ -9,9 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! @@@@@@@@@@@@@@@@@" );
-        System.out.println( "Hello World! 11111111111111111" );
-        System.out.println( "Hello World! 11111111111111111" );
-        System.out.println( "Hello World! 22222222222222222" );
+        System.out.println( "Hello World! old 11111111111111111111" );
+        
     }
 }
