60f2d927974f092fb9eb02b2ac91e4dbb12bbaa0
lp <1078921890@qq.com>
2016-09-10 12:16:20 +0800

    122222

--- gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
+++ gitDemo/src/main/java/com/chefu/git/gitDemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 222222222222" );
+        System.out.println( "Hello World! 3333333333333" );
     }
 }
